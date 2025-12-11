6857c207e5d783866548cb48d0a4bb3b1171beff
ffishstixPC <ffishstix.69@gmail.com>
2025-12-11 19:16:06 +0000

    tiny to set up

--- connection.cs
+++ connection.cs
@@ -11,12 +11,6 @@ public class ConnectionSettings
     private string ipPattern = @"^(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?(?:\.[A-Za-z]{2,63})+|(?:25[0-5]|2[0-4]\d|1?\d{1,2})(?:\.(?:25[0-5]|2[0-4]\d|1?\d{1,2})){3})$";
     //could be either ip or domain so makes it more complicated
     private string portPattern = @"";
-
-    public Tuple<bool, string> isValid()
-    {
-        
-        return new Tuple<bool, string>(true, null);
-    }
 }
 
 
